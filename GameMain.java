108b690302bcdc56d393990d9916f5f859e7d53a
Leah Greenstein <lrg79@cornell.edu>
2016-06-13 16:45:07 -0400

    Create GameMain.java

---
+++ GameMain.java
@@ -0,0 +1,11 @@
+public class GameMain {
+ 
+     public static void main(String[] args) {
+          // TODO Auto-generated method stub
+ 
+         
+          GameCons g= new GameCons();
+          g.preset();
+     }
+ 
+}
